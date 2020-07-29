e3a16300a78f1e6c795853fb1564fbc0c8265aaa
Liza Maslovskaya <maselnp@gmail.com>
2020-07-29 13:16:27 +0300

    do slick

--- src/index.js
+++ src/index.js
@@ -32,3 +32,20 @@ const eventsTabs = new Accordion({
   showClass: 'show',
   cb: ({ target }, index) => console.log(target, index),
 });
+
+// //==== Вариант на JS ===============
+// const navToggle = document.querySelector(".nav-toggle");
+// const mainMenu = document.querySelector(".main-menu");
+// const navToggleLabel = document.querySelector(".nav-toggle-label");
+
+// navToggle.addEventListener("click", () => {
+//     mainMenu.classList.toggle("active");
+//     navToggleLabel.classList.toggle("active");
+// });
+
+// mainMenu.addEventListener("click", (event) => {
+//        if(event.target){
+//           mainMenu.classList.remove("active");
+//           navToggleLabel.classList.remove("active");
+//       }
+// });
