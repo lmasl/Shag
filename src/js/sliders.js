4a7c20551f1578cf0669106614e4d14183d89b12
Liza Maslovskaya <maselnp@gmail.com>
2020-10-15 08:30:07 +0300

    fix:burger

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,37 +1,17 @@
 import $ from "./jquery.min";
 import './slick/slick.min.js';
 
-$('.info').slick({
-  infinite      : false,
-  slidesToShow  : 1,
-  slidesToScroll: 1,
-  variableWidth : true,
-  // adaptiveHeight: true,
-  /*responsive    : [
-    {
-      breakpoint: 1360,
-      settings  : {
-        arrows: false,
-        dots  : true
-      }
-    },
-    {
-      breakpoint: 991,
-      settings  : {
-        arrows        : false,
-        slidesToShow: 2,
-        dots        : true
-      }
-    },
-    {
-      breakpoint: 680,
-      settings  : {
-        arrows        : false,
-        slidesToShow  : 1,
-        slidesToScroll: 1,
-        centerMode    : true,
-        dots          : true
-      }
-    }
-  ]*/
-});
+// $('.single-item').slick({
+//   infinite: false,
+//   arrows:true,
+//   dots: true,
+ 
+// });
+
+// $(document).ready(function(){
+//   $('.single-item').slick({
+//       arrows:true,
+//       dots:true,
+//       adaptiveHeight:true,
+//   })
+// });
